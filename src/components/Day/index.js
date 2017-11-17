9ef540fa9dcdd145dc89c415798f09160acfa8be
Marces Engel <marces.engel@googlemail.com>
2017-11-17 16:16:17 +0000

    Added PropTypes to Day

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import TimeScale from '../TimeScale'
@@ -18,6 +19,16 @@ const Day = (props) => {
 }
 
 export default Day
+
+Day.defaultProps = {
+  steps: 4,
+  stepDuration: 15
+}
+
+Day.propTypes = {
+  steps: PropTypes.number,
+  stepDuration: PropTypes.number
+}
 
 const Wrapper = styled.div`
   position: relative;
