e8abc8a6727bd456d0268a5b98121afc571dd850
Marces Engel <marces.engel@googlemail.com>
2017-11-20 10:42:11 +0100

    Updated Storybook for better test cases

--- stories/storyOf/Calendar/hello-calendar.js
+++ stories/storyOf/Calendar/hello-calendar.js
@@ -4,25 +4,27 @@ import moment from 'moment'
 import Calendar from 'qalendar'
 
 export default () => {
+  const date = moment()
+
   return (
     <Calendar
       defaultView='month'
       events={[
         {
           start: new Date(),
-          end: moment().add(2, 'hour')
+          end: moment(date).add(2, 'hour')
         },
         {
-          start: moment().add(1, 'hour'),
-          end: moment().add(3, 'hour')
+          start: moment(date).add(1, 'hour'),
+          end: moment(date).add(3, 'hour')
         },
         {
-          start: moment().add(2, 'hour'),
-          end: moment().add(4, 'hour')
+          start: moment(date).add(2, 'hour'),
+          end: moment(date).add(4, 'hour')
         },
         {
-          start: moment().add(3.5, 'hour'),
-          end: moment().add(1, 'day')
+          start: moment(date).add(1.5, 'hour'),
+          end: moment(date).add(1, 'day')
         }
       ]} />
   )
